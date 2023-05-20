257d69e62b7ba9ed1c0ee146c029b5909f6d3cbd
liulang <809721414@qq.com>
2023-05-21 00:42:23 +0800

    feat(blog): 新增标签页

---
+++ apps/web/app/tags/[tag]/page.tsx
@@ -0,0 +1,47 @@
+import ArticleItem from '@/components/ArticleItem'
+import { getAllFilesFrontMatter } from '@/libs/mdx'
+import Link from 'next/link'
+import React from 'react'
+
+export const getData = async () => {
+  const posts = await getAllFilesFrontMatter('blog')
+  return { posts }
+}
+
+type Props = {
+  params: {
+    tag: string
+  }
+}
+
+const TagDetailPage = async ({ params }: Props) => {
+  const { posts = [] } = await getData()
+  const tagPosts = posts?.filter((post) => post.tags.includes(params.tag))
+  return (
+    <div>
+      <div className='text-6xl font-black mt-6 pb-6 border-b border-gray-300 border-opacity-50'>
+        {params.tag.toLocaleUpperCase()} （{posts.length}）
+      </div>
+      <div>
+        <ul className='divide-y divide-gray-200 dark:divide-gray-700'>
+          {!posts.length && '无数据'}
+          {tagPosts.map((frontMatter) => {
+            const { slug, date, title, summary, tags } = frontMatter
+            return (
+              <ArticleItem
+                key={slug}
+                slug={slug}
+                title={title}
+                tags={tags}
+                date={date}
+                summary={summary}
+              />
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default TagDetailPage
---
+++ apps/web/app/tags/page.tsx
@@ -0,0 +1,37 @@
+import { getAllFilesFrontMatter } from '@/libs/mdx'
+import Link from 'next/link'
+import React from 'react'
+
+export const getData = async () => {
+  const posts = await getAllFilesFrontMatter('blog')
+  return { tags: posts.map((item) => item.tags) }
+}
+
+const TagsPage = async () => {
+  const { tags } = await getData()
+  const allTags = tags.flat()
+  const tagCount: Record<string, number> = {}
+  allTags.forEach((item) => {
+    if (tagCount?.[item]) {
+      tagCount[item] += 1
+    } else {
+      tagCount[item] = 1
+    }
+  })
+
+  return (
+    <div>
+      <h1 className='text-6xl font-black mt-8 mb-8'>标签🏷</h1>
+      <div className='flex gap-4 flex-wrap'>
+        {Object.entries(tagCount).map(([tag, count]) => (
+          <Link href={`/tags/${tag}`} key={tag} className='inline-block'>
+            <span className='text-primary-400'>{tag.toLocaleUpperCase()}</span>
+            <span>（{count}）</span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default TagsPage
