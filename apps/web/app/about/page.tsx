15aec24a994935735daa6d2016875b0e5dd91875
liulang <809721414@qq.com>
2023-05-21 10:58:39 +0800

    feat(blog): ✨ 新增关于页面

--- apps/web/app/about/page.tsx
+++ apps/web/app/about/page.tsx
@@ -1,27 +1,47 @@
-import { MDXLayoutRenderer } from '@/components/MDXComponents'
-import { getFileBySlug } from '@/libs/mdx'
-import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import { AuthorFrontMatter } from 'types/AuthorFrontMatter'
+import Card from '@/components/Card'
+import projectsData from '@/data/projectsData'
+import Image from 'next/image'
 
-const DEFAULT_LAYOUT = 'AuthorLayout'
-
-// @ts-ignore
-export const getStaticProps: GetStaticProps<{
-  authorDetails: { mdxSource: string; frontMatter: AuthorFrontMatter }
-}> = async () => {
-  const authorDetails = await getFileBySlug<AuthorFrontMatter>('authors', ['default'])
-  const { mdxSource, frontMatter } = authorDetails
-  return { props: { authorDetails: { mdxSource, frontMatter } } }
-}
-
-export default function About({ authorDetails }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const { mdxSource, frontMatter } = authorDetails
-
+export default function Projects() {
   return (
-    <MDXLayoutRenderer
-      layout={frontMatter.layout || DEFAULT_LAYOUT}
-      mdxSource={mdxSource}
-      frontMatter={frontMatter}
-    />
+    <>
+      <div className='divide-y divide-gray-200 dark:divide-gray-700'>
+        <div className='space-y-2 pt-6 pb-8 md:space-y-5'>
+          <h1 className='text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14'>
+            关于
+          </h1>
+        </div>
+        <div className='container py-12'>
+          <div className='flex flex-col items-center gap-4 flex-wrap text-center w-full'>
+            <Image
+              className='rounded-full'
+              src={'/static/images/avatar.jpg'}
+              width={200}
+              height={200}
+              alt={'头像'}
+            />
+            <p className='text-2xl leading-7 font-black'>刘浪</p>
+            <p className='text-gray-500 dark:text-gray-400'>
+              Professor of Atmospheric Science Stanford University mail github linkedin twitter
+            </p>
+            <p className='text-gray-500 dark:text-gray-400'>
+              Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His
+              research interests includes complexity modelling of tailwinds, headwinds and
+              crosswinds.
+            </p>
+            <p className='text-gray-500 dark:text-gray-400'>
+              He leads the clean energy group which develops 3D air pollution-climate models, writes
+              differential equation solvers, and manufactures titanium plated air ballons. In his
+              free time he bakes raspberry pi.
+            </p>
+            <p className='text-gray-500 dark:text-gray-400'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique
+              placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem
+              nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
