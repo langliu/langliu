f91b1c5692dac14839e4774522bc56afd4e74c33
langliu <langliu1216@gmail.com>
2023-06-26 22:49:41 +0800

    feat(blog): ✨ update 透视 HTTP 协议

--- apps/blog/components/Comments.tsx
+++ apps/blog/components/Comments.tsx
@@ -9,12 +9,11 @@ export default function Comments(): JSX.Element {
     <div className='mt-10'>
       <h2 className='text-xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100'>评论</h2>
       <p className='mt-2 text-base text-zinc-600 dark:text-zinc-400 mb-4'>
-        Do you have a problem, want to share feedback, or discuss further ideas? Feel free to leave
-        a comment here! Please stick to English. This comment thread directly maps to a{' '}
+        您是否有问题，想要分享反馈，或者讨论进一步的想法?欢迎在这里留言! 这个评论线程直接映射到{' '}
         <Link href='https://github.com/langliu/langliu/discussions/' target='blank'>
-          <em className='text-teal-500'>discussion on GitHub</em>
+          <em className='text-teal-500'>GitHub上的讨论</em>
         </Link>
-        , so you can also comment there if you prefer.
+        ，所以如果你愿意，你也可以在那里评论。
       </p>
       <Giscus
         id='comments'
