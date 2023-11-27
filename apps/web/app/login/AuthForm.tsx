9307f6eb24409163aeee27abff4fa98e44becef5
langliu <langliu1216@gmail.com>
2023-11-28 00:13:33 +0800

    fix: ✨ 修改路径

--- apps/web/app/login/AuthForm.tsx
+++ apps/web/app/login/AuthForm.tsx
@@ -15,7 +15,7 @@ export default function AuthForm() {
       theme='dark'
       showLinks={false}
       providers={['google', 'twitter', 'github']}
-      redirectTo={`${window.location.origin}/auth/callback`}
+      redirectTo={`${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`}
       localization={{
         variables: {
           sign_in: {
