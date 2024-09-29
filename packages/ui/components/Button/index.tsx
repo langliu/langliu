80d1ec747df79eb6dd122559e10d5c419515e9d0
langliu <langliu1216@gmail.com>
2024-09-30 01:31:28 +0800

    feat: 引入cva作为组件入参于类名进行关联

--- packages/ui/components/Button/index.tsx
+++ packages/ui/components/Button/index.tsx
@@ -1,16 +1,54 @@
 'use client'
-
-import type { FC, PropsWithChildren } from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
+import type { ButtonHTMLAttributes, FC, PropsWithChildren } from 'react'
 import './index.css'
 
-export type ButtonProps = PropsWithChildren<{
-  htmlType?: 'button' | 'submit' | 'reset'
-  type?: 'emphasized' | 'normal'
-}>
+const button = cva('harmony-button', {
+  variants: {
+    variant: {
+      normal: 'harmony-button__normal',
+      emphasized: 'harmony-button__emphasized',
+      warning: 'harmony-button__warning',
+      text: 'harmony-button__text',
+      selected: 'harmony-button__selected',
+      unselected: 'harmony-button__unselected',
+    },
+    shadow: {
+      true: 'harmony-button__shadow',
+    },
+    size: {
+      small: 'harmony-button__small',
+      medium: '',
+    },
+  },
+  defaultVariants: {
+    /** 按钮类型 */
+    variant: 'normal',
+    shadow: false,
+    size: 'medium',
+  },
+})
 
-export const Button: FC<ButtonProps> = ({ children, htmlType = 'button', type = 'normal' }) => {
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof button> &
+  PropsWithChildren<{
+    /** 按钮类型 */
+    variant?: 'normal' | 'emphasized' | 'warning' | 'text' | 'selected' | 'unselected'
+    /** 自定义类名 */
+    className?: string
+    /** 是否展示外阴影 */
+    shadow?: boolean
+  }>
+
+export const Button: FC<ButtonProps> = ({
+  children,
+  variant = 'normal',
+  shadow = false,
+  className,
+  ...props
+}) => {
   return (
-    <button type={htmlType} onClick={() => alert('boop')} className={'button'}>
+    <button className={button({ className, variant, shadow })} {...props}>
       {children}
     </button>
   )
