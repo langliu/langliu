feb15f395683248bcb5c478ca5d7ee559c444fc5
langliu <langliu1216@gmail.com>
2024-10-18 23:25:09 +0800

    style(plan): 添加 Google Analytics 接入

--- apps/plan/src/app/layout.tsx
+++ apps/plan/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import type { ReactNode } from 'react'
 import './globals.css'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -26,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang='zh'>
       <body className={`${geistSans.variable}${geistMono.variable}`}>{children}</body>
+      <GoogleAnalytics gaId={'G-WX4G8HNXYK'} />
     </html>
   )
 }
