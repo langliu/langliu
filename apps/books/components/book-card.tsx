a5cc6e142d8b10425ad64b2e9f0ed0ec5a466de7
langliu <langliu1216@gmail.com>
2024-11-11 22:59:58 +0800

    feat(books): 引入组件

--- apps/books/components/book-card.tsx
+++ apps/books/components/book-card.tsx
@@ -1,19 +1,26 @@
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import Link from 'web/components/Link'
 
 interface BookCardProps {
   title: string
   description: string
+  id: string
+  href: string
 }
 
-export function BookCard({ title, description }: BookCardProps) {
+export function BookCard({ title, description, id, href }: BookCardProps) {
   return (
-    <Card className={'hover:-translate-y-0.5 max-w-[1200px] cursor-pointer hover:translate-x-0.5'}>
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription className={'line-clamp-2 overflow-hidden text-ellipsis'}>
-          {description}
-        </CardDescription>
-      </CardHeader>
-    </Card>
+    <Link href={href}>
+      <Card
+        className={'hover:-translate-y-0.5 max-w-[1200px] cursor-pointer hover:translate-x-0.5'}
+      >
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription className={'line-clamp-2 overflow-hidden text-ellipsis'}>
+            {description}
+          </CardDescription>
+        </CardHeader>
+      </Card>
+    </Link>
   )
 }
---
+++ apps/books/components/dashboard-sidebar.tsx
@@ -0,0 +1,38 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+
+export function DashboardSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>书籍管理</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <a href={'/dashboard/books'}>
+                    <span>书籍管理</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
