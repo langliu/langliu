7ea85d5285688d35d7d2588007ef4ee1faceeb31
langliu <langliu1216@gmail.com>
2024-12-01 23:47:48 +0800

    feat(video): 新增网站导航数据

--- apps/books/app/articles/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1>Hello, Next.js!</h1>
-    </div>
-  )
-}
--- apps/books/app/dashboard/books/create/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function DashboardBookCreatePage() {
-  return (
-    <div>
-      <h1>Create a book</h1>
-    </div>
-  )
-}
